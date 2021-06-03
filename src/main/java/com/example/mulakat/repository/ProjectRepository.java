667b885d2544e5b89d55c2a3b74a767bfb6aa1af
tbayzin <bayzin@hotmail.com.tr>
2021-06-03 23:09:32 +0300

    query imp basl

--- src/main/java/com/example/mulakat/repository/ProjectRepository.java
+++ src/main/java/com/example/mulakat/repository/ProjectRepository.java
@@ -7,15 +7,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,  Integer> {
     Project getProjectById(Integer id);
 
-    @Query("Select SUM(u.salary), pr.name from user_project p left join user u on u.id=p.user_id
-            left join project pr on p.project_id = pr.id
-            group by pr.name")
-
-  //  Collection<Project> getProjectSums();
+    @Query("Select SUM(u.salary), pr.name from user_project p left join user u on u.id=p.user_id " +
+            "            left join project pr on p.project_id = pr.id\n" +
+            "            group by pr.name")
+    List<Project> findBySalarySum();
+    //  Collection<Project> getProjectSums();
 
 }
